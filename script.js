027728b90bdff1fa507c7ca73b8b021831165d6a
Judith Morgan <112726749+judithmorgan@users.noreply.github.com>
2023-10-10 21:37:18 -0400

    added function game

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const game = ['rock', 'paper', 'scissors'];
+const options = ['rock', 'paper', 'scissors'];
 function getComputerChoice() {
-    return game[Math.floor(Math.random() * game.length)];
+    return options[Math.floor(Math.random() * game.length)];
 }
 
 function checkWinner(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
-        return "Tie";
+        return 'Tie';
     } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') ||
         (playerSelection == 'scissors' && computerSelection == 'paper')
     ) {
-        return "Player"
+        return 'Player'
 
     }
     else {
-        return "Computer"
+        return 'Computer'
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     const result = checkWinner(playerSelection, computerSelection);
-    if (result == "Tie") {
+    if (result == 'Tie') {
         return "It's a Tie!"
     }
-    else if (result === "Player") {
+    else if (result === 'Player') {
         return `You Win! ${playerSelection} beats ${computerSelection}`
     }
     else {
@@ -30,9 +30,16 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
 
+function game() {
+    console.log("Welcome!")
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = 'rock';
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
 
 
