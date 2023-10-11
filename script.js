3c944f580fed69bf605a5fcd93a9a13bfb704870
Judith Morgan <112726749+judithmorgan@users.noreply.github.com>
2023-10-10 21:03:20 -0400

    added function checkWinner

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ function getComputerChoice() {
     return game[Math.floor(Math.random() * game.length)];
 }
 
+function checkWinner(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "Tie";
+    } else if ((playerSelection == 'rock' && computerSelection == 'scissors') || (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')
+    ) {
+        return "You Win and Computer Lose!! YAYYYY"
+
+    }
+    else {
+        return "You Lose and Computer Wins! BOOO"
+    }
+}
+
 console.log(getComputerChoice());
 
 
- 
